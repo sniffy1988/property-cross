1dc4caef7aec80ae90d10f4a43bbd8e245a73a33
Sniffy1988 <Sniffy1988@gmail.com>
2016-04-06 12:52:50 +0300

    Add searchByloc method;

--- src/app/components/nestoriaAPI/nestoria.service.js
+++ src/app/components/nestoriaAPI/nestoria.service.js
@@ -1,27 +1,34 @@
 export class NestoriaAPI {
-  constructor ($http, $q) {
+  constructor($http, $q) {
     'ngInject';
     const baseUrl = ['http://api.nestoria.co.uk/api?',
       'country=uk&pretty=1&action=search_listings',
       '&encoding=json&listing_type=buy&page=1&',
       'callback=JSON_CALLBACK&place_name='
     ].join('');
+    const locUrl = ['http://api.nestoria.co.uk/api?',
+      'country=uk&pretty=1&action=search_listings',
+      '&encoding=json&listing_type=buy&page=1&',
+      'callback=JSON_CALLBACK&centre_point='
+    ].join('');
+    const fakeLoc = '51.684183,-3.431481';
 
-    const validCodes = [ '100', '101', '110' ],
-      ambiguousCodes = [ '200', '202' ];
 
-    this.get = function(text) {
+    const validCodes = ['100', '101', '110'],
+      ambiguousCodes = ['200', '202'];
+
+    this.get = function (text) {
       var deferred = $q.defer();
 
       $http.jsonp(baseUrl + text)
-        .then(function(response) {
+        .then(function (response) {
           let code = response.data.response.application_response_code;
-          if (validCodes.indexOf(code) > -1 ) {
+          if (validCodes.indexOf(code) > -1) {
             deferred.resolve({
               type: 'listings',
               data: response.data.response.listings
             });
-          } else if(ambiguousCodes.indexOf(code) > -1) {
+          } else if (ambiguousCodes.indexOf(code) > -1) {
             deferred.resolve({
               type: 'locations',
               data: response.data.response.locations
@@ -33,5 +40,28 @@ export class NestoriaAPI {
 
       return deferred.promise;
     };
+    this.searchByLocation = function (lat, lng) {
+      var defer = $q.defer();
+      $http.jsonp(locUrl + lat + ',' + lng)
+        .then(function (response) {
+          let code = response.data.response.application_response_code;
+          if (validCodes.indexOf(code) > -1) {
+            deferred.resolve({
+              type: 'listings',
+              data: response.data.response.listings
+            });
+          } else if (ambiguousCodes.indexOf(code) > -1) {
+            deferred.resolve({
+              type: 'locations',
+              data: response.data.response.locations
+            });
+          } else {
+            deferred.reject(response.data);
+          }
+        }, deferred.reject);
+
+      return deferred.promise;
+    }
   }
+
 }
